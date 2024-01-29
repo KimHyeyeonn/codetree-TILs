f3240ce233924f97e8da293e65cceaa1c070d13e
KimHyeyeonn <85014114+KimHyeyeonn@users.noreply.github.com>
2024-01-29 14:44:24 +0900

    Title: 체질량지수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240129/체질량지수/bmi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner ;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cm = sc.nextInt();
+        int kg = sc.nextInt();
+
+        //int bmi = kg/((100/cm)*(100/cm)) ;
+          int bmi = kg * 100 * 100 / (cm * cm);
+
+        if(bmi >= 25){
+            System.out.println(bmi);
+            System.out.println("Obesity");
+        }else{
+            System.out.println(bmi);
+        }
+    }
+}
