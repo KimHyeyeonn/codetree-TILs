7e766693fb26d7bea3f4a7da772c890058c64e29
KimHyeyeonn <85014114+KimHyeyeonn@users.noreply.github.com>
2024-01-30 15:45:51 +0900

    Title: 특정 조건 세 정수 비교, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240130/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240130/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -1,36 +1,25 @@
 import java.util.Scanner;
 
-// 3개의 정수를 입력 받아 첫 번째 수가 세 정수의 최솟값과 
-// 일치한다면 1 아니면 0을 출력하고, 
-// 세 개의 수가 모두 같으면 1 아니면 0 을 출력하는 프로그램을 작성해주세요.
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
 
-        if(a>b && b>c && a==c){
-            System.out.print(1 + " ");
-        }
-        else if(a>c && c>b && a==b){
-            System.out.print(1+ " ");
-        }
-        else if (b>c && c>a ){
-            System.out.print(1+ " ");
-        } 
-        else if (c>b && b>a ){
-            System.out.print(1+ " ");
-        }
-        else{
-            System.out.print(0+ " ");
-        }
+        // 변수 선언
+        int a, b, c;
+        
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        
+        if(a <= b && a <= c)
+            System.out.print("1 ");
+        else
+            System.out.print("0 ");
 
-        if (a == b && b == c) {
-            System.out.print(1+ " ");
-        }
-        else {
-            System.out.print(0+ " ");
-        }
+        if(a == b && b == c)
+            System.out.println("1");
+        else
+            System.out.println("0");
     }
 }
