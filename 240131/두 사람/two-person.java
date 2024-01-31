0e89cedd56b4c2eae46b27e8dfa12074f9a5fe34
KimHyeyeonn <85014114+KimHyeyeonn@users.noreply.github.com>
2024-01-31 19:16:40 +0900

    Title: 두 사람, Time: 158ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240131/두 사람/two-person.java
+++ 240131/두 사람/two-person.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         // 입력
         int age1 = sc.nextInt();
         String fm1 = sc.next();
@@ -11,13 +11,10 @@ public class Main {
         int age2 = sc.nextInt();
         String fm2 = sc.next();
 
-        if(age1 >= 19 || age2 >= 19){
-            System.out.println( fm1.equals("M") || fm2.equals("M") ? 1 : 0);
+        if(fm1.equals("M") || fm2.equals("M")) {
+            System.out.print(age1 >= 19 || age2 >= 19 ? 1 : 0);
         }else{
             System.out.println(0);
         }
-
     }
-        
-            
 }
