da7a3e67e9500bff8cfb97933b95ac35d843a8be
KimHyeyeonn <85014114+KimHyeyeonn@users.noreply.github.com>
2024-02-16 09:30:36 +0900

    Title: 별표 출력하기 3, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240216/별표 출력하기 3/print-star-3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        // n번째 줄까지의 별표 출력
+        for (int i = n; i > 0; i--) {
+            // 현재 줄 전에 공백 출력
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("  ");
+            }
+            
+            // 현재 줄의 별표 출력
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
